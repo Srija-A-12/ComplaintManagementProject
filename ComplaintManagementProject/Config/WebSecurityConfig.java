0ca49a4f403238abbceb51d410443c126b64ba9b
Srija A <srijaadusumalli02@gmail.com>
2025-09-19 22:07:29 +0530

    Update WebSecurityConfig.java

--- ComplaintManagementProject/Config/WebSecurityConfig.java
+++ ComplaintManagementProject/Config/WebSecurityConfig.java
@@ -1,58 +1,3 @@
-//package com.example.ComplaintManagementProject.Config;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.util.matcher.AnyRequestMatcher;
-//
-//import com.example.ComplaintManagementProject.Service.AuthencationService;
-//
-//@Configuration
-//public class WebSecurityConfig {
-//	
-//	@Autowired
-//	private AuthencationService authencationService;
-//	
-//	@Bean
-//	public BCryptPasswordEncoder passwordEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
-//	
-//	@Bean
-//	public AuthenticationManager authenticationManager(HttpSecurity http, BCryptPasswordEncoder encoder) throws Exception {
-//        return http.getSharedObject(AuthenticationManagerBuilder.class)
-//                   .userDetailsService(authencationService)
-//                   .passwordEncoder(encoder)
-//                   .and()
-//                   .build();
-//    }
-//	@Bean
-//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//		http.csrf().disable()
-//		.authorizeHttpRequests()
-//		.requestMatchers("/login","register").permitAll()
-//		.anyRequest().authenticated()
-//		.and()
-//		.formLogin()
-//		.loginPage("/login")
-//		.defaultSuccessUrl("/dashboard",true)
-//		.permitAll()
-//		.and()
-//		.logout()
-//		.permitAll();
-//		
-//		return http.build();
-//		
-//		
-//	}
-//
-//}
-
 package com.example.ComplaintManagementProject.Config;
 
 
@@ -136,5 +81,6 @@ public class WebSecurityConfig {
         };
     }
 }
+
 
 
